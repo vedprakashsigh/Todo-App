9ba00072c0e2e6682032c48114655e93621e7de7
vedprakashsigh <vedprakashsigh@gmail.com>
2023-05-08 12:38:00 +0530

    Todo List: Basic Structure Complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [todoList, setTodoList] = useState([]);
+  const [newTasks, setNewTasks] = useState("");
+  return (
+    <div className="App">
+      <div className="addTask">
+        <input
+          type="text"
+          onChange={(event) => {
+            setNewTasks(event.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            setTodoList([...todoList, newTasks]);
+          }}
+        >
+          Add Task
+        </button>
+        <button
+          onClick={() => {
+            setTodoList([]);
+          }}
+        >
+          Clear All
+        </button>
+      </div>
+      <div className="list">
+        {todoList.map((task) => {
+          return <p>{task}</p>;
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
